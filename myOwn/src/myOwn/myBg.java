fd33b770b899fb887aaea2cfdade7bfcbfb16938
Akshay Goel <agl@saxobank.com>
2015-06-11 16:36:55 +0530

    final

---
+++ myOwn/src/myOwn/myBg.java
@@ -0,0 +1,45 @@
+package myOwn;
+
+public class myBg {
+
+	int bgx,bgy,bgSpeedx,bgSpeedy;
+	
+	public void calculatePosition(){
+		
+	}
+
+	public int getBgx() {
+		return bgx;
+	}
+
+	public void setBgx(int bgx) {
+		this.bgx = bgx;
+	}
+
+	public int getBgy() {
+		return bgy;
+	}
+
+	public void setBgy(int bgy) {
+		this.bgy = bgy;
+	}
+
+	public int getBgSpeedx() {
+		return bgSpeedx;
+	}
+
+	public void setBgSpeedx(int bgSpeedx) {
+		this.bgSpeedx = bgSpeedx;
+	}
+
+	public int getBgSpeedy() {
+		return bgSpeedy;
+	}
+
+	public void setBgSpeedy(int bgSpeedy) {
+		this.bgSpeedy = bgSpeedy;
+	}
+	
+	
+	
+}
